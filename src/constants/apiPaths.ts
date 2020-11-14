3502c682da80a5114ea155c8dc71de9e0b50c78e
Ivan Prasmytski <ivan.presmytsky@gmail.com>
2020-11-14 20:29:40 +0300

    add new api endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   products: 'https://2s5xprqt4k.execute-api.eu-west-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://mmd5l7u8kl.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
