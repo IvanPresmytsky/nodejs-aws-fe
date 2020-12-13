52c96e9d3a2e3c58583c02a427e718c2063c589d
Ivan Prasmytski <ivan.presmytsky@gmail.com>
2020-12-13 16:21:02 +0300

    update api endpoints with http proxy

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
-  products: 'http://ivanpresmytky-bff-api-dev.eu-west-1.elasticbeanstalk.com/products',
+  products: 'https://fjzs1we9h1.execute-api.eu-west-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://mmd5l7u8kl.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://hrxry5b1t8.execute-api.eu-west-1.amazonaws.com/dev/',
-  cart: 'http://ivanpresmytky-bff-api-dev.eu-west-1.elasticbeanstalk.com',
+  cart: 'https://fjzs1we9h1.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
