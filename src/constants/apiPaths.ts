e2885eb69c0c833c86d826206de503e8295d2243
Ivan Prasmytski <ivan.presmytsky@gmail.com>
2020-12-13 15:33:44 +0300

    update api endpoints for cart and products

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
-  products: 'https://hrxry5b1t8.execute-api.eu-west-1.amazonaws.com/dev/products',
+  products: 'http://ivanpresmytky-bff-api-dev.eu-west-1.elasticbeanstalk.com/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://mmd5l7u8kl.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://hrxry5b1t8.execute-api.eu-west-1.amazonaws.com/dev/',
-  cart: 'https://ineqj4uhab.execute-api.eu-west-1.amazonaws.com/dev/api',
+  cart: 'http://ivanpresmytky-bff-api-dev.eu-west-1.elasticbeanstalk.com',
 };
 
 export default API_PATHS;
